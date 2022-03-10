a8dd243b84340d5bb4885eaf3436d9167b895e23
valflrt <vfleurit@gmail.com>
2022-03-10 09:05:51 +0100

    add Layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,16 @@ import React, { FunctionComponent } from "react";
 import "./Layout.css";
 
 const Layout: FunctionComponent = ({ children }) => {
-  return <>{children}</>;
+  return (
+    <>
+      <h1>
+        meumeu<span className="dot">.</span>
+      </h1>
+      <div className="card-wrapper">
+        <div className="card">{children}</div>
+      </div>
+    </>
+  );
 };
 
 export default Layout;
