3b58eb0bbe153dbe976736e6a9482b265722d9e1
val <vfleurit@gmail.com>
2022-03-10 16:55:41 +0000

    add a grid

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 
-const Home = () => <div>Home !</div>;
+import "./Home.css";
+
+const Home = () => (
+  <div>
+    Home !
+    <div className="grid">
+      <img src="https://en.wiktionary.org/wiki/File:LetterA.svg" alt="nope" />
+    </div>
+  </div>
+);
 
 export default Home;
