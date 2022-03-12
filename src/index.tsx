4498383ff409d3fa6aa47fb48811b6c05be03b3f
valflrt <vfleurit@gmail.com>
2022-03-12 19:30:01 +0100

    edit index.html

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Layout>
         <Router />
       </Layout>
