7b9612386b1eee6ee82e1b2d3d33d76b7bb9d258
valflrt <vfleurit@gmail.com>
2022-03-13 17:28:47 +0100

    rework drawing - add auto image sizing

--- src/routes/Drawing.tsx
+++ src/routes/Drawing.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 
 import drawings from "../assets/drawings.json";
@@ -7,20 +7,45 @@ import "./Drawing.css";
 
 const Drawing = () => {
   let params = useParams();
-  console.log(params);
+
+  let drawingRef = useRef<HTMLImageElement | null>(null);
+  let [width, setWidth] = useState<number | undefined>(0);
+  useEffect(() => {
+    setWidth(drawingRef.current ? drawingRef.current.offsetWidth : undefined);
+  }, [drawingRef.current]);
+
   let drawing = drawings.find((d) => d.id == params.id);
-  console.log(drawing);
+
   return (
     <>
       <Link className="link" to={"/"}>
         Back
       </Link>
       {drawing ? (
-        <img
-          className="drawing"
-          src={drawing.url}
-          alt={`Drawing #${drawing.id}`}
-        />
+        <>
+          <div className="drawing-wrapper" id="drawing-wrapper">
+            <img
+              className="drawing"
+              ref={drawingRef}
+              src={
+                width
+                  ? drawing.url
+                      .replace("cdn.discordapp.com", "media.discordapp.net")
+                      .concat(
+                        `?width=${width}&height=${Math.round(
+                          (width * drawing.original.height) /
+                            drawing.original.width
+                        ).toFixed()}`
+                      )
+                  : drawing.url
+              }
+              alt={`Drawing #${drawing.id}`}
+            />
+          </div>
+          <a className="link" href={drawing.url} target="_blank">
+            See full image
+          </a>
+        </>
       ) : (
         "Unknown drawing !"
       )}
