6371dbedd51ce24a9a2a2c5d0a46af70c7a7d7d4
valflrt <vfleurit@gmail.com>
2022-03-13 16:18:25 +0100

    add hash router base path

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={`${process.env.PUBLIC_URL}#/`}>
       <Layout>
         <Router />
       </Layout>
