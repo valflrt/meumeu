caa12f106133135df712e885a942c787828c7e96
valflrt <vfleurit@gmail.com>
2022-03-13 19:49:45 +0100

    edit Home

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,30 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
-
-import drawings from "../assets/drawings.json";
 
 import "./Home.css";
 
-const Home = () => (
-  <div>
-    Hello !
-    <div className="grid">
-      {drawings.map((d) => (
-        <Link key={d.id} to={`/drawing/${d.id}`}>
-          <img
-            src={d.url
-              .replace("cdn.discordapp.com", "media.discordapp.net")
-              .concat(
-                `?width=${Math.round(
-                  (150 * d.original.width) / d.original.height
-                ).toFixed()}&height=150`
-              )}
-            alt={`Drawing #${d.id}`}
-          />
-        </Link>
-      ))}
-    </div>
-  </div>
-);
+const Home = () => {
+  return <div>Hello !</div>;
+};
 
 export default Home;
