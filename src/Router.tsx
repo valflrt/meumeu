c97ab071e2ba32471c13ce17302c40202e827957
valflrt <vfleurit@gmail.com>
2022-03-13 20:05:11 +0100

    fix Router

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,14 +3,14 @@ import { Route, Routes } from "react-router-dom";
 
 import Home from "./routes/Home";
 import Arts from "./routes/Arts";
-import Drawing from "./routes/Drawing";
+import Art from "./routes/Art";
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/arts/" element={<Arts />} />
-      <Route path="/art/:id" element={<Drawing />} />
+      <Route path="/art/:id" element={<Art />} />
       <Route path="*" element={"There is nothing here !"} />
     </Routes>
   );
