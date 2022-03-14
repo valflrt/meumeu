a38fa2be6dc3cfe65405391b2ca492f92f79869e
val <vfleurit@gmail.com>
2022-03-14 12:54:25 +0000

    add some routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 import Home from "./routes/Home";
+import About from "./routes/About";
+import Contact from "./routes/Contact";
 import Arts from "./routes/Arts";
 import Art from "./routes/Art";
 
@@ -9,6 +11,8 @@ const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
       <Route path="/arts/" element={<Arts />} />
       <Route path="/art/:id" element={<Art />} />
       <Route path="*" element={"There is nothing here !"} />
