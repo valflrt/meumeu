4a091bfcd6cbce91234f11582a2060718fb8418a
val <vfleurit@gmail.com>
2022-03-14 12:54:43 +0000

    edit TopPanel

--- src/components/TopPanel.tsx
+++ src/components/TopPanel.tsx
@@ -1,28 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
-import "./TopPanel.css";
+import {
+  LinksDivStyle,
+  LinkStyle,
+  TitleStyle,
+  ThaiDivStyle,
+  TopPanelStyle,
+  MainTitleStyle,
+} from "./TopPanelStyles";
 
 const TopPanel = () => (
-  <div className="top-panel">
-    <h1 className="main-title">
-      <Link to={"/"}>meumeu.</Link>
-    </h1>
-    <div className="links">
-      <Link className="link" to={"/"}>
-        Home
-      </Link>
-      <Link className="link" to={"/arts"}>
-        Arts
-      </Link>
-      <Link className="link" to={"/about"}>
-        About
-      </Link>
-      <Link className="link" to={"/contact"}>
-        Contact
-      </Link>
-    </div>
-  </div>
+  <TopPanelStyle>
+    <MainTitleStyle to={"/"}>
+      <TitleStyle>meumeu.</TitleStyle>
+      <ThaiDivStyle>
+        <span>เ</span>
+        <span>มิ</span>
+        <span>อ</span>
+        <span>ะ</span>
+        <span>ๆ</span>
+      </ThaiDivStyle>
+    </MainTitleStyle>
+    <LinksDivStyle>
+      <LinkStyle to={"/"}>Home</LinkStyle>
+      <LinkStyle to={"/arts"}>Arts</LinkStyle>
+      <LinkStyle to={"/about"}>About</LinkStyle>
+      <LinkStyle to={"/contact"}>Contact</LinkStyle>
+    </LinksDivStyle>
+  </TopPanelStyle>
 );
 
 export default TopPanel;
---
+++ src/components/TopPanelStyles.tsx
@@ -0,0 +1,50 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+export let TopPanelStyle = styled.div`
+  width: 100%;
+  padding: 10px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export let MainTitleStyle = styled(Link)`
+  padding: 5px;
+
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+  justify-content: center;
+`;
+
+export let TitleStyle = styled.h1`
+  margin: 0;
+  font-size: 48px;
+  font-family: "Sansita Swashed";
+`;
+
+export let ThaiDivStyle = styled.div`
+  width: 120px;
+
+  font-family: "Pridi";
+  font-size: 18px;
+  line-height: 8px;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export let LinksDivStyle = styled.div`
+  padding-top: 16px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+export let LinkStyle = styled(Link)`
+  padding: 0 4px;
+`;
