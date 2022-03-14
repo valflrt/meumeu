8b600457a21e6d3252679366fadbc3c6783e2b71
val <vfleurit@gmail.com>
2022-03-14 12:55:08 +0000

    edit Home

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./Home.css";
 
 const Home = () => {
-  return <div>Hello !</div>;
+  return (
+    <>
+      <p>
+        Wanna{" "}
+        <Link className="link" to="/about">
+          learn more
+        </Link>{" "}
+        about me :) ?
+      </p>
+    </>
+  );
 };
 
 export default Home;
