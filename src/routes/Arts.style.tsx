85821dd300278804074b195f377d8fbbccd5c910
val <vfleurit@gmail.com>
2022-03-17 16:26:02 +0000

    edit Art styles

--- src/routes/Arts.style.tsx
+++ src/routes/Arts.style.tsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
+import ArtThumbnailStyles from "../components/ArtThumbnail.styles";
+
 let Grid = styled.div`
   width: 100%;
 
@@ -8,6 +10,18 @@ let Grid = styled.div`
   justify-content: center;
 
   gap: 6px;
+
+  ${ArtThumbnailStyles.Thumbnail} {
+    width: 160px;
+    height: 160px;
+  }
+
+  @media screen and (max-width: 500px) {
+    ${ArtThumbnailStyles.Thumbnail} {
+      width: 120px;
+      height: 120px;
+    }
+  }
 `;
 
 export default {
