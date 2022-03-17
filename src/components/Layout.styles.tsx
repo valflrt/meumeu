505c30d19996214f11d6350059e7761246ff4522
valflrt <vfleurit@gmail.com>
2022-03-17 09:08:57 +0100

    edit Layout styles

--- src/components/Layout.styles.tsx
+++ src/components/Layout.styles.tsx
@@ -2,7 +2,8 @@ import styled from "styled-components";
 
 export let MainWrapper = styled.div`
   width: 100%;
-  height: 100%;
+
+  padding: 20px;
 
   display: flex;
   flex-direction: column;
@@ -13,7 +14,6 @@ export let MainWrapper = styled.div`
 
 export let ContentWrapper = styled.div`
   width: 500px;
-  height: 100%;
 
   padding: 20px 0;
 
