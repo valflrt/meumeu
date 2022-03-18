0e1578de83a670106751bdb6d184478833507656
valflrt <vfleurit@gmail.com>
2022-03-18 17:52:35 +0100

    remove useless imports

--- src/routes/Art.tsx
+++ src/routes/Art.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
 import CommonStyles from "../common.styles";
